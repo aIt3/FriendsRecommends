a2bb45c7ca6e14fb53d92e53a231f57489c3491a
alt3 <alt3@github.com>
2022-09-12 17:20:24 +0200

    ERR currentUser of undefined get displayed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,9 +5,7 @@ const saltRounds = 10;
 const { Router } = require('express');
 const router = new Router();
  
-// GET route ==> to display the signup form to users
-
-// POST route ==> to process form data
+//// R E G I S T R A T I O N 
 router.post('/signup', (req, res, next) => {
     const { username, email, password } = req.body;
 
@@ -28,4 +26,52 @@ router.post('/signup', (req, res, next) => {
       .catch(error => next(error));
   
   });
+//// L O G I N 
+
+router.get('/userProfile', (req, res) => {
+
+    res.render('users/user-profile', { userInSession: req.session.currentUser });
+  
+  });
+
+router.post('/login', (req, res, next) => {
+    console.log('SESSION =====> ', req.session);
+        const { email, password } = req.body;
+        if (email === '' || password === '') {
+          res.render('auth/login', {
+            errorMessage: 'Please enter both, email and password to login.'
+          });
+          return;
+        }
+        User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+            return;
+          } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+            // when we introduce session, the following line gets replaced with what follows:
+            // res.render('users/user-profile', { user });
+     
+            //******* SAVE THE USER IN THE SESSION ********//
+            req.session.currentUser = user;
+            res.redirect('/userProfile');
+                  } else {
+            res.render('auth/login', { errorMessage: 'Incorrect password.' });
+          }
+        })
+        .catch(error => next(error));
+    });
+
+//// L O G O U T 
+
+router.post('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+      if (err) next(err);
+      res.redirect('/');
+    });
+  
+  });
+  
+   
+  
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,6 +6,8 @@ router.get("/", (req, res, next) => {
 });
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
+router.get('/login', (req, res) => res.render('auth/login'));
+
 
 // POST route ==> to process form data
 
