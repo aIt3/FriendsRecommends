8cecfc676b8a6fa57e4741c1f7c7281a758fe912
alt3 <alt3@github.com>
2022-09-13 09:34:21 +0200

    Login and Logout works

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+
+
+router.get('/', (req, res) => {
+  res.render('index', { userInSession: req.session.currentUser });
+
 });
+
+
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 router.get('/login', (req, res) => res.render('auth/login'));
