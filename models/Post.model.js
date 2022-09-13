dace01b51f0918938eeb934704c6657de0f7b51d
alt3 <alt3@github.com>
2022-09-13 10:19:12 +0200

    created Posts Model

---
+++ models/Post.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+	link: String,
+	title: String,
+    postedByUser: String,
+    dateAdded: Date, 
+    mediaType: String, 
+    topic: String,
+	Reactions: {
+		type: Schema.Types.ObjectId,
+		// this refers to the model the id above belongs to
+		ref: 'User',
+        emoji: String
+	}
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
