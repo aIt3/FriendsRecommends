67851cd5cce28e72b0625dd9de9ece32106146ac
Calvins Soonen Christopher <calvinskris@gmail.com>
2022-09-15 15:52:30 +0300

    Thumbsup count created to backend

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,22 @@ document.addEventListener(
   },
   false
 );
+
+const button = document.getElementById('thumbsUp');
+button.addEventListener('click', function(e) {
+  const postId=e.target.getAttribute('data-postId')
+
+  fetch(`/clicked/${postId}`, {method: 'POST'})
+    .then(function(response) {
+      if(response.ok) {
+        console.log('click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+ 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -26,4 +26,30 @@ router.get('/login', (req, res) => res.render('auth/login'));
 
 // POST route ==> to process form data
 
+// get the click data from the database
+router.post('/clicked/:postId', (req, res) => {
+  console.log(req.params.postId)
+  Post.findByIdAndUpdate(req.params.postId,{$inc: {thumbsUp: 1}},{new:true}).then(updated=>{console.log(updated)})
+  
+  // db.collection('clicks').find().toArray((err, result) => {
+  //   if (err) return console.log(err);
+  //   res.send(result);
+  // });
+});
+
+// add a document to the DB collection recording the click event
+  // app.post('/clicked', (req, res) => {
+  //   const click = {clickTime: new Date()};
+  //   console.log(click);
+  //   console.log(db);
+  
+  //   db.collection('clicks').save(click, (err, result) => {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log('click added to db');
+  //     res.sendStatus(201);
+  //   });
+  // });
+
 module.exports = router;
